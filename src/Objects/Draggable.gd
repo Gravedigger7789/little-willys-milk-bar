ee9a27be9b02461d58d2f1a1a7dbbfd3e1f570f6
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-07 00:58:51 -0500

    Added shadow to draggable objects

--- src/Objects/Draggable.gd
+++ src/Objects/Draggable.gd
@@ -4,17 +4,49 @@ signal picked_up
 
 export var outline_color := Color.white
 
-onready var sprite := $Sprite
+onready var sprite: Sprite = $Sprite
+onready var sprite_start_scale := sprite.scale
+onready var sprite_lift_scale := sprite_start_scale + Vector2(0.1, 0.1)
+onready var sprite_shadow: Sprite = $SpriteShadow
+onready var sprite_shadow_start_position := sprite_shadow.position
 
 var _dragging := false
+var _touch_position_offset := Vector2()
+var _shadow_lift_offset := Vector2(10, 10)
 
-func _physics_process(_delta: float) -> void:
+
+func _ready() -> void:
+	sprite_shadow.texture = sprite.texture
+
+
+func _process(_delta: float) -> void:
 	if _dragging:
-		position = get_global_mouse_position()
+		var new_position = get_global_mouse_position() - _touch_position_offset
+		var tween := create_tween() \
+			.set_trans(Tween.TRANS_SINE) \
+			.set_ease(Tween.EASE_OUT)
+		# warning-ignore:return_value_discarded
+		tween.tween_property(self, "global_position", new_position, 0.15)
+		if sprite.scale == sprite_start_scale:
+			# warning-ignore:return_value_discarded
+			tween.parallel().tween_property(sprite, "scale", sprite_lift_scale, 0.15)
+		if sprite_shadow.position == sprite_shadow_start_position:
+			# warning-ignore:return_value_discarded
+			tween.parallel().tween_property(sprite_shadow, "position", sprite_shadow.position + _shadow_lift_offset, 0.15)
+	else:
+		if sprite.scale != sprite_start_scale or sprite_shadow.position != sprite_shadow_start_position:
+			var tween := create_tween() \
+				.set_trans(Tween.TRANS_SINE) \
+				.set_ease(Tween.EASE_OUT)
+			# warning-ignore:return_value_discarded
+			tween.tween_property(sprite, "scale", sprite_start_scale, 0.15)
+			# warning-ignore:return_value_discarded
+			tween.parallel().tween_property(sprite_shadow, "position", sprite_shadow_start_position, 0.15)
 
 
 func _input(event: InputEvent) -> void:
-	if _dragging and event.is_action_released("touch"):
+	if _dragging and (event.is_action_released("touch") or \
+		(event is InputEventScreenTouch and !event.is_pressed())):
 		_dragging = false
 
 
@@ -23,6 +55,7 @@ func _on_Draggable_input_event(_viewport: Node, event: InputEvent, _shape_idx: i
 		get_tree().set_input_as_handled()
 		emit_signal("picked_up", self)
 		_dragging = true
+		_touch_position_offset = get_global_mouse_position() - global_position
 
 
 func _on_Draggable_mouse_entered() -> void:
@@ -31,6 +64,7 @@ func _on_Draggable_mouse_entered() -> void:
 		.set_ease(Tween.EASE_OUT)
 	# warning-ignore:return_value_discarded
 	tween.tween_method(self, "_outline_alpha", 0.0, 1.0, 0.5)
+
 
 func _on_Draggable_mouse_exited() -> void:
 	var tween := create_tween() \
