9a209c6d0b7172a7a9756b0459d613c015b4578b
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-07 23:38:46 -0500

    use draggable class

--- src/Objects/Coaster.gd
+++ src/Objects/Coaster.gd
@@ -6,13 +6,13 @@ onready var sprite: Sprite = $Sprite
 onready var snap_position: Position2D = $SnapPosition
 
 
-func _on_Coaster_area_entered(area: Area2D) -> void:
+func _on_Coaster_area_entered(area: Draggable) -> void:
 	if (!area.is_connected("put_down", self, "_on_Draggable_put_down")):
 		var _connected = area.connect("put_down", self, "_on_Draggable_put_down")
 	_tween_outline(0.0, 1.0)
 
 
-func _on_Coaster_area_exited(area: Area2D) -> void:
+func _on_Coaster_area_exited(area: Draggable) -> void:
 	if (area.is_connected("put_down", self, "_on_Draggable_put_down")):
 		area.disconnect("put_down", self, "_on_Draggable_put_down")
 	_tween_outline(1.0, 0.0)
@@ -31,5 +31,5 @@ func _set_outline_alpha(value: float) -> void:
 	sprite.material.set_shader_param("outline_color", outline_color)
 
 
-func _on_Draggable_put_down(area: Area2D) -> void:
+func _on_Draggable_put_down(area: Draggable) -> void:
 	area.snap_to_position(snap_position.global_position)
