a8b79694d8afef34f76c80a9629991213d57ca15
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-07 21:39:37 -0500

    removed duplicated tween

--- src/Objects/Coaster.gd
+++ src/Objects/Coaster.gd
@@ -9,23 +9,21 @@ onready var snap_position: Position2D = $SnapPosition
 func _on_Coaster_area_entered(area: Area2D) -> void:
 	if (!area.is_connected("put_down", self, "_on_Draggable_put_down")):
 		var _connected = area.connect("put_down", self, "_on_Draggable_put_down")
-	
-	var tween := create_tween() \
-		.set_trans(Tween.TRANS_LINEAR) \
-		.set_ease(Tween.EASE_OUT)
-	# warning-ignore:return_value_discarded
-	tween.tween_method(self, "_outline_alpha", 0.0, 1.0, 0.5)
+	_tween_outline(0.0, 1.0)
 
 
 func _on_Coaster_area_exited(area: Area2D) -> void:
 	if (area.is_connected("put_down", self, "_on_Draggable_put_down")):
 		area.disconnect("put_down", self, "_on_Draggable_put_down")
-	
+	_tween_outline(1.0, 0.0)
+
+
+func _tween_outline(from: float, to: float):
 	var tween := create_tween() \
 		.set_trans(Tween.TRANS_LINEAR) \
 		.set_ease(Tween.EASE_OUT)
 	# warning-ignore:return_value_discarded
-	tween.tween_method(self, "_outline_alpha", 1.0, 0.0, 0.5)
+	tween.tween_method(self, "_outline_alpha", from, to, 0.5)
 
 
 func _outline_alpha(value: float) -> void:
--- src/Objects/Draggable.gd
+++ src/Objects/Draggable.gd
@@ -66,19 +66,19 @@ func _on_Draggable_input_event(_viewport: Node, event: InputEvent, _shape_idx: i
 
 
 func _on_Draggable_mouse_entered() -> void:
-	var tween := create_tween() \
-		.set_trans(Tween.TRANS_LINEAR) \
-		.set_ease(Tween.EASE_OUT)
-	# warning-ignore:return_value_discarded
-	tween.tween_method(self, "_outline_alpha", 0.0, 1.0, 0.5)
+	_tween_outline(0.0, 1.0)
 
 
 func _on_Draggable_mouse_exited() -> void:
+	_tween_outline(1.0, 0.0)
+
+
+func _tween_outline(from: float, to: float):
 	var tween := create_tween() \
 		.set_trans(Tween.TRANS_LINEAR) \
 		.set_ease(Tween.EASE_OUT)
 	# warning-ignore:return_value_discarded
-	tween.tween_method(self, "_outline_alpha", 1.0, 0.0, 0.5)
+	tween.tween_method(self, "_outline_alpha", from, to, 0.5)
 
 
 func _outline_alpha(value: float) -> void:
