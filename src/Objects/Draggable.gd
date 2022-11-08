0fdbfec37532e039ea426b01cd90411d8cbab065
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-07 21:34:10 -0500

    Added snap position

--- src/Objects/Draggable.gd
+++ src/Objects/Draggable.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal picked_up
+signal put_down
 
 export var outline_color := Color.white
 
@@ -9,6 +10,8 @@ onready var sprite_start_scale := sprite.scale
 onready var sprite_lift_scale := sprite_start_scale + Vector2(0.1, 0.1)
 onready var sprite_shadow: Sprite = $SpriteShadow
 onready var sprite_shadow_start_position := sprite_shadow.position
+onready var snap_position: Position2D = $SnapPosition
+
 
 var _dragging := false
 var _touch_position_offset := Vector2()
@@ -47,7 +50,10 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if _dragging and (event.is_action_released("touch") or \
 		(event is InputEventScreenTouch and !event.is_pressed())):
+		get_tree().set_input_as_handled()
 		_dragging = false
+		monitorable = false
+		emit_signal("put_down", self)
 
 
 func _on_Draggable_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
@@ -56,6 +62,7 @@ func _on_Draggable_input_event(_viewport: Node, event: InputEvent, _shape_idx: i
 		emit_signal("picked_up", self)
 		_dragging = true
 		_touch_position_offset = get_global_mouse_position() - global_position
+		monitorable = true
 
 
 func _on_Draggable_mouse_entered() -> void:
@@ -77,3 +84,11 @@ func _on_Draggable_mouse_exited() -> void:
 func _outline_alpha(value: float) -> void:
 	outline_color.a = value
 	sprite.material.set_shader_param("outline_color", outline_color)
+
+
+func snap_to_position(value: Vector2) -> void:
+	var tween := create_tween() \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_OUT)
+	# warning-ignore:return_value_discarded
+	tween.tween_property(self, "global_position", value - snap_position.position, 0.15)
