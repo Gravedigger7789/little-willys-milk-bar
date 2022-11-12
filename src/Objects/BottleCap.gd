53b9736bbe24e7d1a701594524a65c1419b13b24
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-11 23:28:12 -0500

    Hooking up sprites

--- src/Objects/BottleCap.gd
+++ src/Objects/BottleCap.gd
@@ -1,1 +1,22 @@
+tool
 extends Draggable
+
+
+export(String, "Red", "Blue", "Green") var color: String = "Blue" setget set_color
+
+const color_dict := { \
+	"Red" : preload("res://assets/bottle/bottle-lid-red.png"), \
+	"Blue" : preload("res://assets/bottle/bottle-lid-blue.png"), \
+	"Green" : preload("res://assets/bottle/bottle-lid-green.png"), \
+	}
+
+
+func _ready() -> void:
+	set_color(color)
+
+
+func set_color(value: String) -> void:
+	color = value
+	if is_inside_tree():
+		sprite.texture = color_dict[color]
+		sprite_shadow.texture = color_dict[color]
