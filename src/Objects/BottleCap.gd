340c6438fe6c458a362c4ae1bf8f8562a0cab5ee
Chris Racioppo <Gravedigger7789@gmail.com>
2022-11-12 00:51:03 -0500

    Changing bottle size

--- src/Objects/BottleCap.gd
+++ src/Objects/BottleCap.gd
@@ -4,7 +4,7 @@ extends Draggable
 
 export(String, "Red", "Blue", "Green") var color: String = "Blue" setget set_color
 
-const color_dict := { \
+const COLOR_DICT := { \
 	"Red" : preload("res://assets/bottle/bottle-lid-red.png"), \
 	"Blue" : preload("res://assets/bottle/bottle-lid-blue.png"), \
 	"Green" : preload("res://assets/bottle/bottle-lid-green.png"), \
@@ -18,5 +18,5 @@ func _ready() -> void:
 func set_color(value: String) -> void:
 	color = value
 	if is_inside_tree():
-		sprite.texture = color_dict[color]
-		sprite_shadow.texture = color_dict[color]
+		sprite.texture = COLOR_DICT[color]
+		sprite_shadow.texture = COLOR_DICT[color]
