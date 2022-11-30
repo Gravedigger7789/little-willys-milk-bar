ad8d1719f818deb107295a6ff4ff616b2c3eb1af
Gravedigger7789 <Gravedigger7789@users.noreply.github.com>
2022-11-30 00:50:50 +0000

    deploy: 8824ea85eea83500f399fbef11991da38da5f2d7

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1669611065|6836439";
+const CACHE_VERSION = "1669769428|7293978";
 const CACHE_PREFIX = "baby-saloon-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
