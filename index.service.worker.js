b296cbb9ae5b376df58dee97493c8421cc986a50
Gravedigger7789 <Gravedigger7789@users.noreply.github.com>
2022-11-30 02:23:14 +0000

    deploy: 845b4a4838b73960acf2c1580be2e2bdc1ac4bac

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1669769853|7983893";
+const CACHE_VERSION = "1669774976|7540669";
 const CACHE_PREFIX = "Llittle Willy's -sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
